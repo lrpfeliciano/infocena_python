45d595b1571aea50631ab5a26dcc6ebf490770f4
Excel Avançado 2019.6 <39117022019.6@senacrjedu.local>
2025-05-07 21:45:47 -0300

    Alterar e Excluir

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
             name='listar_lojas'),
             
     path('incluir_loja', views.incluir_loja,
-            name='incluir_loja'),            
+            name='incluir_loja'),   
+
+    path('alterar_loja/<int:codigo>', views.alterar_loja,
+            name='alterar_loja'),   
+
+    path('excluir_loja/<int:codigo>', views.excluir_loja,
+            name='excluir_loja'),          
 ]
 
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from cadastro.forms import LojaForm
 from .models import Loja
+from django.contrib import messages
+from django.db.models.functions import Lower
 
 # Create your views here.
 
@@ -10,7 +12,7 @@ def index(request):
     return HttpResponse("Olá Mundo! Agora é web")
 
 def listar_lojas(request):
-    lojas = Loja.objects.order_by('nome')
+    lojas = Loja.objects.order_by(Lower('nome'))
     return render(request, 'listar_lojas.html',
                   {'lojas':lojas })
 
@@ -20,7 +22,29 @@ def incluir_loja(request):
         if form.is_valid():
             form.save()
             return listar_lojas(request)
+            # msg = 'Registro cadastrado com sucesso'
+            # return render(request, 'incluir_loja.html',
+            #         {'mensagem': msg, 'formulario': form})
     form = LojaForm()
     return render(request, 'incluir_loja.html',
                   {'formulario': form})
+
+def alterar_loja(request, codigo):
+    loja = Loja.objects.get(id=codigo)
+
+    if request.method == 'POST':
+        form = LojaForm(request.POST, instance=loja)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_lojas')
+    form = LojaForm(instance=loja)
+    return render(request, 'incluir_loja.html',
+                  {'formulario': form})
+
+def excluir_loja(request, codigo):
+    loja = Loja.objects.get(id=codigo)
+
+    Loja.delete(loja)
+
+    return redirect('listar_lojas')
 
